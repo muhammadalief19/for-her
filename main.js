b297fc39ce64ee1fb1e8b79b8331b00c4aadc634
muhammmadalief19 <aliefmuhammad1908@gmail.com>
2023-04-26 04:16:36 +0700

    change song

--- main.js
+++ main.js
@@ -1,74 +1,86 @@
-function syncLyric(lyrics, time) {
-  const scores = [];
-  lyrics.forEach((lyric) => {
-    const score = time - lyric.time;
-    if (score >= 0) scores.push(score);
-  });
-  if (scores.length == 0) return null;
-  const closest = Math.min(...scores);
-  return scores.indexOf(closest);
-}
+// song
+const listSong = ["surat-cinta-untuk-starla", "cupid", "until-i-found-you"];
 
-function parseLyric(lrc) {
-  const regex = /^\[(?<time>\d{2}:\d{2}(.\d{2})?)\](?<text>.*)/;
-  const lines = lrc.split("\n");
-  const output = [];
-  lines.forEach((line) => {
-    const match = line.match(regex);
-    if (match == null) return;
-    const { time, text } = match.groups;
-    output.push({
-      time: parseTime(time),
-      text: text.trim(),
+window.onload = () => {
+  let length = listSong.length;
+  let index = Math.floor(Math.random() * length);
+  let song = listSong[index];
+  console.log(song);
+
+  // lirik
+  function syncLyric(lyrics, time) {
+    const scores = [];
+    lyrics.forEach((lyric) => {
+      const score = time - lyric.time;
+      if (score >= 0) scores.push(score);
     });
-  });
-
-  function parseTime(time) {
-    const minsec = time.split(":");
-    const min = parseInt(minsec[0]) * 60;
-    const sec = parseFloat(minsec[1]);
-    return min + sec;
-  }
-  return output;
-}
-
-const preloader = document.querySelector(".preloader");
-const playBtn = document.getElementById("play");
-const progress = document.querySelector(".progress");
-const music = new Audio();
-music.src = "./assets/surat-cinta-untuk-starla.mp3";
-playBtn.addEventListener("click", playing);
-preloader.style.display = "none";
-
-async function playing() {
-  "use strict";
-
-  const lyric = document.querySelector(".lyric");
-  const res = await fetch("./assets/lyric-virgoun.lrc");
-  const lrc = await res.text();
-  const lyrics = parseLyric(lrc);
-
-  if (music.paused) {
-    music.play();
-    playBtn.setAttribute("class", "fa fa-pause");
-  } else {
-    music.pause();
-    playBtn.setAttribute("class", "fa fa-play");
+    if (scores.length == 0) return null;
+    const closest = Math.min(...scores);
+    return scores.indexOf(closest);
   }
 
-  music.ontimeupdate = () => {
-    const time = music.currentTime;
-    const index = syncLyric(lyrics, time);
+  function parseLyric(lrc) {
+    const regex = /^\[(?<time>\d{2}:\d{2}(.\d{2})?)\](?<text>.*)/;
+    const lines = lrc.split("\n");
+    const output = [];
+    lines.forEach((line) => {
+      const match = line.match(regex);
+      if (match == null) return;
+      const { time, text } = match.groups;
+      output.push({
+        time: parseTime(time),
+        text: text.trim(),
+      });
+    });
 
-    if (index == null) return;
+    function parseTime(time) {
+      const minsec = time.split(":");
+      const min = parseInt(minsec[0]) * 60;
+      const sec = parseFloat(minsec[1]);
+      return min + sec;
+    }
+    return output;
+  }
 
-    lyric.innerHTML = lyrics[index].text;
-  };
-}
+  // music
+  const preloader = document.querySelector(".preloader");
+  const playBtn = document.getElementById("play");
+  const progress = document.querySelector(".progress");
+  const music = new Audio();
+  music.src = `./assets/${song}/song.mp3`;
+  playBtn.addEventListener("click", playing);
+  preloader.style.display = "none";
 
-music.addEventListener("timeupdate", updateDurationMusic);
+  async function playing() {
+    "use strict";
 
-function updateDurationMusic() {
-  let newDuration = music.currentTime * (100 / music.duration);
-  progress.style.height = `${newDuration}%`;
-}
+    const lyric = document.querySelector(".lyric");
+    const res = await fetch(`./assets/${song}/lyric.lrc`);
+    const lrc = await res.text();
+    const lyrics = parseLyric(lrc);
+
+    if (music.paused) {
+      music.play();
+      playBtn.setAttribute("class", "fa fa-pause");
+    } else {
+      music.pause();
+      playBtn.setAttribute("class", "fa fa-play");
+    }
+
+    music.ontimeupdate = () => {
+      const time = music.currentTime;
+      const index = syncLyric(lyrics, time);
+
+      if (index == null) return;
+
+      lyric.innerHTML = lyrics[index].text;
+    };
+  }
+
+  music.addEventListener("timeupdate", updateDurationMusic);
+
+  function updateDurationMusic() {
+    let newDuration = music.currentTime * (100 / music.duration);
+    progress.style.height = `${newDuration}%`;
+  }
+};
